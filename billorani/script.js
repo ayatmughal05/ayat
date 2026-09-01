2ab4316e3c25feae644af93688f17ac07ce3fc89
ayatmughal05 <thedesifitnessfuel@gmail.com>
2026-09-01 14:38:00 -0500

    Delete billorani directory

--- billorani/script.js
+++
@@ -1,7 +0,0 @@
-const cat = document.getElementById("billi");
-const music = document.getElementById("music");
-
-cat.addEventListener("click", () => {
-  music.currentTime = 0.5; // start from desired second
-  music.play();
-});
